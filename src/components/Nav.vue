7d31f51d6b455b66926388e16573b1712e8e0a99
jameshuang623 <363753466@qq.com>
2021-08-27 15:03:25 +0800

    html 和 css 完成

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -25,8 +25,8 @@ name: "Nav"
 <style scoped lang="scss">
 @import "src/assets/style/helper";
 nav{
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0 ,0.25);
   font-size: 12px;
   > .item{
     padding: 2px 0;
